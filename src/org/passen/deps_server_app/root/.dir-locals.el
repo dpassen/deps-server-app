3200ac44d4be454c7706ff7070810388d5a48a08
Derek Passen <dpassen1@gmail.com>
2022-05-18 20:40:13 -0500

    Customize compile-command

--- src/org/passen/deps_server_app/root/.dir-locals.el
+++ src/org/passen/deps_server_app/root/.dir-locals.el
@@ -1,4 +1,6 @@
-((clojure-mode
+((nil
+  (compile-command . "just build"))
+ (clojure-mode
   (cider-clojure-cli-aliases . "dev:test")
   (cider-ns-refresh-before-fn . "user/stop")
   (cider-ns-refresh-after-fn . "user/go*")))
