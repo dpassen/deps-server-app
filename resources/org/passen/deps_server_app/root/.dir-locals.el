e1e99b11e4077c590ec22b87b38b94d72eae145d
Derek Passen <dpassen1@gmail.com>
2023-07-07 17:12:13 -0500

    Set logging framework for CIDER

--- resources/org/passen/deps_server_app/root/.dir-locals.el
+++ resources/org/passen/deps_server_app/root/.dir-locals.el
@@ -2,5 +2,6 @@
   (compile-command . "task build"))
  (clojure-mode
   (cider-clojure-cli-aliases . ":dev:test")
+  (cider-log-framework-name . "Logback")
   (cider-ns-refresh-before-fn . "user/stop")
   (cider-ns-refresh-after-fn . "user/go*")))
