dff53177e896ee429f5dac91c30b050e58501291
Derek Passen <dpassen1@gmail.com>
2026-02-07 17:37:44 -0600

    No longer using `clojure-ts-mode`

--- resources/org/passen/deps_server_app/root/.dir-locals.el
+++ resources/org/passen/deps_server_app/root/.dir-locals.el
@@ -1,6 +1,6 @@
 ((nil
   (compile-command . "just build"))
- (clojure-ts-mode
+ (clojure-mode
   (cider-clojure-cli-aliases . ":dev:test")
   (cider-log-framework-name . "Logback")
   (cider-ns-refresh-before-fn . "user/stop")
